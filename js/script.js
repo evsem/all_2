595455c8c9379ee1041dd6d36860d9e5c381f2f3
evsem <frukwork@gmail.com>
2021-12-04 20:26:23 +0300

    v(js_1)-all_2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log('Hello World')
+
+let allServer = {
+  server: 'aex-par/',
+  status: false,
+  port: 4700,
+  adressServer: {
+    country: 'France',
+    town: 'Lion',
+  },
+  infoServer: {
+    dev: 'evsem',
+    dateBirth: '27.09.19',
+    lang: ['JS', 'Python'],
+  },
+}
+
+const str_Hello = 'Hello'
+localStorage.setItem('String_1', str_Hello)
+
+let a = 89
+const b = 34
+for (let i = b; i < 89; i++) {
+  if (i == 47) {
+    break
+  }
+  //console.log(i)
+}
+
+for (let p = 13; p > 7; p--) {
+  console.log([...p])
+}
